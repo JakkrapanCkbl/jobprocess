2c75e8db75b663d2ccbd6d859fefc414cf5b7a8c
voranitha <maymelodyyy12@gmail.com>
2021-01-13 20:37:40 +0700

    change hero, menubar, icon, and footer

--- resources/views/dashboard/menubar.blade.php
+++ resources/views/dashboard/menubar.blade.php
@@ -4,7 +4,224 @@
 						<div class="horizontal-mainwrapper container clearfix">
 							<nav class="horizontalMenu clearfix">
 								<ul class="horizontalMenu-list">
+									<li aria-haspopup="true"><a href="#" class="sub-icon active"><i class="fe fe-home"></i> Job <i class="fa fa-angle-down horizontal-icon"></i></a>
+										<ul class="sub-menu">
+											<li aria-haspopup="true"><a href="#">Work in Progress</a></li>
+											<li aria-haspopup="true"><a href="#">Jobs to Done</a></li>
+											<li aria-haspopup="true"><a href="#">LC Big Data</a></li>
+											
+										</ul>
+									</li>
+									<li aria-haspopup="true"><a href="#" class="sub-icon active"><i class="fe fe-home"></i> Valuer/Checker <i class="fa fa-angle-down horizontal-icon"></i></a>
+										<ul class="sub-menu">
+											<li aria-haspopup="true"><a href="#">ผังเมือง</a></li>
+											<li aria-haspopup="true"><a href="#">กฎหมายควบคุมอาคาร</a></li>
+											<li aria-haspopup="true"><a href="#">โครงการพัฒนา/เวนคืนรัฐบาล</a></li>
+											<li aria-haspopup="true"><a href="#">ตรวจสอบป่าไม้</a></li>
+											<li aria-haspopup="true"><a href="#">เสาไฟฟ้าแรงสูง</a></li>
+											<li aria-haspopup="true"><a href="#">ค่าก่อสร้างทางราชการ</a></li>
+											<li aria-haspopup="true"><a href="#">ค่าก่อสร้างของสมาคม</a></li>
+											<li aria-haspopup="true"><a href="#">ต้นทุนค่าก่อสร้าง LC</a></li>
+											<li aria-haspopup="true"><a href="#">วัสดุก่อสร้าง</a></li>
+											
+										</ul>
+									</li>
+									<li aria-haspopup="true"><a href="#" class="sub-icon active"><i class="fe fe-home"></i> ตัวอย่างรายงาน <i class="fa fa-angle-down horizontal-icon"></i></a>
+										<ul class="sub-menu">
+											<li aria-haspopup="true"><a href="#">1. ห้องชุด</a></li>
+											<li aria-haspopup="true"><a href="#">2. บ้านเดี่ยว</a></li>
+											<li aria-haspopup="true"><a href="#">3. บ้านแฝด</a></li>
+											<li aria-haspopup="true"><a href="#">4. ทาวน์เฮ้าส์/ทาวน์โฮม</a></li>
+											<li aria-haspopup="true"><a href="#">5. ตึกแถว</a></li>
+											<li aria-haspopup="true"><a href="#">6. ที่ดินว่างเปล่า</a></li>
+											<li aria-haspopup="true"><a href="#">7. โรงงาน/โกดัง</a></li>
+											<li aria-haspopup="true"><a href="#">8. อพาร์ทเม้นท์</a></li>
+											<li aria-haspopup="true"><a href="#">9. โรงแรม</a></li>
+											<li aria-haspopup="true"><a href="#">10. อาคารสำนักงาน</a></li>
+											<li aria-haspopup="true"><a href="#">11. โฮมออฟฟิศ/มินิออฟฟิศ</a></li>
+											<li aria-haspopup="true"><a href="#">12. ที่ดินพร้อมสิ่งปลูกสร้าง</a></li>
+											<li aria-haspopup="true"><a href="#">13. สิทธิการเช่า</a></li>
+											<li aria-haspopup="true"><a href="#">14. เครื่องจักร</a></li>
+											<li aria-haspopup="true"><a href="#">15. อื่นๆ</a></li>
+											
+										</ul>
+									</li>
+									<li aria-haspopup="true"><a href="#" class="sub-icon active"><i class="fe fe-home"></i> Template <i class="fa fa-angle-down horizontal-icon"></i></a>
+										<ul class="sub-menu">
+											<li aria-haspopup="true"><a href="#" class="sub-icon active"> Appendix </a>
+												<ul class="sub-menu">
+													<li aria-haspopup="true"><a href="#" class="sub-icon active">1. CD</a>
+														<ul class="sub-menu">
+															<li aria-haspopup="true"><a href="#">แผนที่</a></li>
+															<li aria-haspopup="true"><a href="#">ภาพถ่ายทางอากาศ</a></li>
+															<li aria-haspopup="true"><a href="#">ผังโครงการ</a></li>
+															<li aria-haspopup="true"><a href="#">แปลนชั้น</a></li>
+															<li aria-haspopup="true"><a href="#">แปลนห้องชุด</a></li>
+														</ul>
+													</li>
+													<li aria-haspopup="true"><a href="#" class="sub-icon active">2. DH/SDH/TW/TH</a>
+														<ul class="sub-menu">
+															<li aria-haspopup="true"><a href="#">แผนที่</a></li>
+															<li aria-haspopup="true"><a href="#">ภาพถ่ายทางอากาศ</a></li>
+															<li aria-haspopup="true"><a href="#">ผังเมือง</a></li>
+															<li aria-haspopup="true"><a href="#">ผังโครงการ</a></li>
+															<li aria-haspopup="true"><a href="#">ผังที่ดิน</a></li>
+															<li aria-haspopup="true"><a href="#">ถนนภาระจำยอมระวางที่ดิน</a></li>
+															<li aria-haspopup="true"><a href="#">ผังบริเวณ</a></li>
+															<li aria-haspopup="true"><a href="#">แปลนอาคาร</a></li>
+														</ul>
+													</li>
+													<li aria-haspopup="true"><a href="#" class="sub-icon active">3. VL</a>
+														<ul class="sub-menu">
+															<li aria-haspopup="true"><a href="#">แผนที่</a></li>
+															<li aria-haspopup="true"><a href="#">ภาพถ่ายทางอากาศ</a></li>
+															<li aria-haspopup="true"><a href="#">ผังเมือง</a></li>
+															<li aria-haspopup="true"><a href="#">ผังที่ดิน</a></li>
+															<li aria-haspopup="true"><a href="#">ถนนภาระจำยอมระวางที่ดิน</a></li>
+														</ul>
+													</li>
+													<li aria-haspopup="true"><a href="#" class="sub-icon active">4. FA</a>
+														<ul class="sub-menu">
+															<li aria-haspopup="true"><a href="#">แผนที่</a></li>
+															<li aria-haspopup="true"><a href="#">ภาพถ่ายทางอากาศ</a></li>
+															<li aria-haspopup="true"><a href="#">ผังเมือง</a></li>
+															<li aria-haspopup="true"><a href="#">ผังที่ดิน</a></li>
+															<li aria-haspopup="true"><a href="#">ถนนภาระจำยอมระวางที่ดิน</a></li>
+															<li aria-haspopup="true"><a href="#">ผังบริเวณ</a></li>
+															<li aria-haspopup="true"><a href="#">แปลนอาคาร</a></li>
+														</ul>
+													</li>
+													<li aria-haspopup="true"><a href="#" class="sub-icon active">5. HP(สภาพเป็นที่ดินว่างเปล่า)</a>
+														<ul class="sub-menu">
+															<li aria-haspopup="true"><a href="#">แผนที่</a></li>
+															<li aria-haspopup="true"><a href="#">ภาพถ่ายทางอากาศ</a></li>
+															<li aria-haspopup="true"><a href="#">ผังเมือง</a></li>
+															<li aria-haspopup="true"><a href="#">ผังโครงการ</a></li>
+															<li aria-haspopup="true"><a href="#">ผังที่ดิน</a></li>
+															<li aria-haspopup="true"><a href="#">ถนนภาระจำยอมระวางที่ดิน</a></li>
+															<li aria-haspopup="true"><a href="#">แปลนอาคาร</a></li>
+														</ul>
+													</li>
+													<li aria-haspopup="true"><a href="#" class="sub-icon active">6. HP(กำลังพัฒนาโครงการ)</a>
+														<ul class="sub-menu">
+															<li aria-haspopup="true"><a href="#">แผนที่</a></li>
+															<li aria-haspopup="true"><a href="#">ภาพถ่ายทางอากาศ</a></li>
+															<li aria-haspopup="true"><a href="#">ผังเมือง</a></li>
+															<li aria-haspopup="true"><a href="#">ผังโครงการ/Work-in-Progress</a></li>
+															<li aria-haspopup="true"><a href="#">ผังที่ดิน</a></li>
+															<li aria-haspopup="true"><a href="#">ถนนภาระจำยอมระวางที่ดิน</a></li>
+															<li aria-haspopup="true"><a href="#">แปลนอาคาร</a></li>
+														</ul>
+													</li>
+												</ul>
+											</li>
+											<li aria-haspopup="true"><a href="#">Photo</a>
+												<ul class="sub-menu">
+													<li aria-haspopup="true"><a href="#" class="sub-icon active">1. CD</a>
+														<ul class="sub-menu">
+															<li aria-haspopup="true"><a href="#">ถนนหลักซอย</a></li>
+															<li aria-haspopup="true"><a href="#">ด้านหน้าอาคารชุด</a></li>
+															<li aria-haspopup="true"><a href="#">บริเวณส่วนกลาง</a></li>
+															<li aria-haspopup="true"><a href="#">ทางเดินหน้าห้องชุด</a></li>
+															<li aria-haspopup="true"><a href="#">ภายในห้องชุด</a></li>
+															<li aria-haspopup="true"><a href="#">วิว</a></li>
+															<li aria-haspopup="true"><a href="#">Data</a></li>
+														</ul>
+													</li>
+													<li aria-haspopup="true"><a href="#" class="sub-icon active">2. DH/SDH/TW/TH</a>
+														<ul class="sub-menu">
+															<li aria-haspopup="true"><a href="#">ถนนหลักซอย</a></li>
+															<li aria-haspopup="true"><a href="#">ด้านหน้าโครงการ</a></li>
+															<li aria-haspopup="true"><a href="#">บริเวณส่วนกลาง</a></li>
+															<li aria-haspopup="true"><a href="#">ถนนด้านหน้าอาคาร</a></li>
+															<li aria-haspopup="true"><a href="#">ด้านหน้าอาคาร</a></li>
+															<li aria-haspopup="true"><a href="#">ภายในอาคาร</a></li>
+															<li aria-haspopup="true"><a href="#">Data</a></li>
+														</ul>
+													</li>
+													<li aria-haspopup="true"><a href="#" class="sub-icon active">3. VL</a>
+														<ul class="sub-menu">
+															<li aria-haspopup="true"><a href="#">ถนนหลักถนนรองซอย</a></li>
+															<li aria-haspopup="true"><a href="#">ด้านหน้าด้านข้างด้านหลังที่ดิน</a></li>
+															<li aria-haspopup="true"><a href="#">ฝั่งตรงข้ามที่ดิน</a></li>
+															<li aria-haspopup="true"><a href="#">ภายในแปลนที่ดินทั่วทั้งแปลน</a></li>
+															<li aria-haspopup="true"><a href="#">Data</a></li>
+														</ul>
+													</li>
+													<li aria-haspopup="true"><a href="#" class="sub-icon active">4. FA</a>
+														<ul class="sub-menu">
+															<li aria-haspopup="true"><a href="#">ถนนหลักถนนรองซอย</a></li>
+															<li aria-haspopup="true"><a href="#">ด้านหน้าด้านข้างด้านหลังที่ดิน</a></li>
+															<li aria-haspopup="true"><a href="#">ฝั่งตรงข้ามที่ดิน</a></li>
+															<li aria-haspopup="true"><a href="#">ภายในทรัพย์สิน</a></li>
+															<li aria-haspopup="true"><a href="#">อาคารและสิ่งปลูกสร้างอื่นๆ</a></li>
+															<li aria-haspopup="true"><a href="#">Data</a></li>
+														</ul>
+													</li>
+													<li aria-haspopup="true"><a href="#" class="sub-icon active">5. HP(สภาพเป็นที่ดินว่างเปล่า)</a>
+														<ul class="sub-menu">
+															<li aria-haspopup="true"><a href="#">ถนนหลักถนนรองซอย</a></li>
+															<li aria-haspopup="true"><a href="#">ด้านหน้าด้านข้างด้านหลังที่ดิน</a></li>
+															<li aria-haspopup="true"><a href="#">ฝั่งตรงข้ามที่ดิน</a></li>
+															<li aria-haspopup="true"><a href="#">ภายในแปลงที่ดินทั่วทั้งแปลง</a></li>
+															<li aria-haspopup="true"><a href="#">อาคารและสิ่งปลูกสร้างอื่นๆ</a></li>
+															<li aria-haspopup="true"><a href="#">Data</a></li>
+														</ul>
+													</li>
+													<li aria-haspopup="true"><a href="#" class="sub-icon active">6. HP(กำลังพัฒนาโครงการ)</a>
+														<ul class="sub-menu">
+														<li aria-haspopup="true"><a href="#">ถนนหลักถนนรองซอย</a></li>
+															<li aria-haspopup="true"><a href="#">ด้านหน้าด้านข้างด้านหลังที่ดิน</a></li>
+															<li aria-haspopup="true"><a href="#">ฝั่งตรงข้ามที่ดิน</a></li>
+															<li aria-haspopup="true"><a href="#">ภายในแปลงที่ดิน</a></li>
+															<li aria-haspopup="true"><a href="#">อาคารและสิ่งปลูกสร้างอื่นๆ</a></li>
+															<li aria-haspopup="true"><a href="#">Data</a></li>
+														</ul>
+													</li>
+												</ul>
+											</li>
+											<li aria-haspopup="true"><a href="#">Visio</a></li>
+											<li aria-haspopup="true"><a href="#">ใบปะหน้า Appendix</a></li>
+											
+										</ul>
+									</li>
+									<li aria-haspopup="true"><a href="#" class="sub-icon active"><i class="fe fe-home"></i> E-library <i class="fa fa-angle-down horizontal-icon"></i></a>
+										<ul class="sub-menu">
+											<li aria-haspopup="true"><a href="#">VDO-การประเมิน</a></li>
+											<li aria-haspopup="true"><a href="#">VDO-อสังหาริมทรัพย์</a></li>
+											<li aria-haspopup="true"><a href="#">VDO/PODCAST สัมมนา</a></li>
+											<li aria-haspopup="true"><a href="#">เอกสารประกอบสัมมนา</a></li>
+											<li aria-haspopup="true"><a href="#">กฎหมายที่ดิน</a></li>
+											<li aria-haspopup="true"><a href="#">กฎหมายเกี่ยวกับการประเมิน</a></li>
+											<li aria-haspopup="true"><a href="#">เอกสารประกอบการสอบวิชาชีพ</a></li>
+											<li aria-haspopup="true"><a href="#">อื่นๆ</a></li>
+											
+										</ul>
+									</li>
 									<li aria-haspopup="true"><a href="#" class="sub-icon active"><i class="fe fe-home"></i> Dashboard <i class="fa fa-angle-down horizontal-icon"></i></a>
+										<ul class="sub-menu">
+											<li aria-haspopup="true"><a href="#">Job in progress</a></li>
+											<li aria-haspopup="true"><a href="#">Jobs to Done</a></li>
+											<li aria-haspopup="true"><a href="#">KPI-Valuer</a></li>
+											<li aria-haspopup="true"><a href="#">Income</a></li>
+											<li aria-haspopup="true"><a href="#">Cost</a></li>
+											<li aria-haspopup="true"><a href="#">Profit</a></li>
+											<li aria-haspopup="true"><a href="#">Breakeven</a></li>
+											<li aria-haspopup="true"><a href="#">Working Capital</a></li>
+											<li aria-haspopup="true"><a href="#">Potential Jobs</a></li>
+											<li aria-haspopup="true"><a href="#">SEC Report</a></li>
+											
+										</ul>
+									</li>
+									<li aria-haspopup="true"><a href="#" class="sub-icon active"><i class="fe fe-home"></i> Admin <i class="fa fa-angle-down horizontal-icon"></i></a>
+										<ul class="sub-menu">
+											<li aria-haspopup="true"><a href="#">Order for Service</a></li>
+											<li aria-haspopup="true"><a href="#">Invoice</a></li>
+											<li aria-haspopup="true"><a href="#">Tax Receipt</a></li>
+											
+										</ul>
+									</li>
+									<!--li aria-haspopup="true"><a href="#" class="sub-icon active"><i class="fe fe-home"></i> Dashboard <i class="fa fa-angle-down horizontal-icon"></i></a>
 										<ul class="sub-menu">
 											<li aria-haspopup="true"><a href="{{route('welcome')}}">Home</a></li>
 											<li aria-haspopup="true"><a href="{{route('dashboard')}}">Dashboard</a></li>
@@ -56,7 +273,7 @@
 											</li>
 											
 										</ul>
-									</li>
+									</li-->
 								
 									
 									
--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -2,13 +2,13 @@
   <div class="container">
     <div class="row align-items-center">
       <div class="col-12" data-aos="fade">
-        <h1>Find Property</h1>
+        <!--h1>Find Property</h1-->
         <form action="{{route('alljobs')}}">
           <div class="row mb-3">
             <div class="col-md-9">
               <div class="row">
                 <div class="col-md-12 mb-3 mb-md-0">
-                  <input type="text" name="search" class="mr-3 form-control border-0 px-4" placeholder="title, keywords or project name ">
+                  <input type="text" name="search" class="mr-3 form-control border-0 px-4" placeholder="ค้นหา : ชื่อคอนโด, ชื่อหมู่บ้าน, ชื่อถนน, ชื่ออำเภอ, ...">
                 </div>
                 <!--<div class="col-md-6 mb-3 mb-md-0">
                       <div class="input-wrap">
@@ -18,10 +18,10 @@
                     </div>-->
               </div>
             </div>
-            <div class="col-md-3">
+            <!--div class="col-md-3">
               <input type="submit" class="btn btn-search btn-primary btn-block" value="Search">
             </div>
-          </div>
+          </div-->
 
 
 
